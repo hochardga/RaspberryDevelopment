496a5e9244c8746dca5d17ff2159cef2dbc12815
Greg Hochard <hochardga@gmail.com>
2015-11-17 02:02:24 +0000

    SenseHat Additions

--- sorting-hat/extra-sorting-hat.py
+++ sorting-hat/extra-sorting-hat.py
@@ -13,8 +13,16 @@ def lightshow():
     time.sleep(1)
     pibrella.light.off() 
 
+def hmm():
+    times = random.randint(0, 3)
+    for _ in range(times):
+        hmm_number = random.randint(1, 3)
+        os.system('mpg123 hmm%d.mp3' % hmm_number)
+        time.sleep(0.33)    
+
 def randomgenerator():
-    lightshow()
+    #lightshow()
+    hmm()
     number = random.randint(1, 4)
 
     if number == 1:
